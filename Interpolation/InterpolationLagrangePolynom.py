18fbe856598a01a009ad6e86763c1739d5e30baa
N1kSt4r <nikita2000tsuranov@gmail.com>
2019-09-07 20:25:52 +0300

    add integrating

--- Interpolation/InterpolationLagrangePolynom.py
+++ Interpolation/InterpolationLagrangePolynom.py
@@ -3,12 +3,11 @@ import scipy as sp
 from scipy.misc import derivative
 import matplotlib.pyplot as plt
 
-n = 11
+n = 5
 a, b = 1, 2
 f = lambda x: 0.8 * np.log(x) + 0.6 * np.cos(x)
 x = np.linspace(a, b, n)
 y = f(x)
-
 
 w = lambda arg: np.prod(arg - x)
 dw = lambda arg, k: np.prod(arg - np.append(x[:k], x[k+1:]))
@@ -21,10 +20,10 @@ def P(arg):
 
 def R(arg):
     max_derivative = max([
-        derivative(lambda x: sp.power(x, 5), n=2, x0=1, dx=0.00001)
+        abs(derivative(f, n=n+1, x0=1, dx=0.1, order=n//2 * 2 + 5))
         for i in np.linspace(a, b, n * 100)
     ])
-    return w(arg) * max_derivative / np.prod(np.arange(n + 1) + 1)
+    return abs(w(arg) * max_derivative / np.prod(np.arange(n + 1) + 1))
 
 xRange = np.linspace(a - (b - a) / 10, b + (b - a) / 10, (b - a) * 100)
 print("Value at (b + a)/5: %f" % P((b + a) / 5))
